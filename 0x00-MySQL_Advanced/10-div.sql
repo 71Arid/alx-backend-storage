11bae6872ce69951edd45193bd5c54338a6b1855
71Arid <no232el@gmail.com>
2024-03-24 22:25:06 +0300

    safe div

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,14 @@
+-- safe divison function
+DELIMITER $$
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+BEGIN
+	DECLARE result FLOAT;
+	IF b = 0 THEN
+		SET result = 0;
+	ELSE
+		SET result = a / b;
+	END IF;
+	RETURN result;
+END$$
+DELIMITER ;
