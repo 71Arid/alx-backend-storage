607e9cbde6745b865c147bb8846f7845deadb66a
71Arid <no232el@gmail.com>
2024-03-26 23:44:08 +0300

    update topucs

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+changes all topics of a school based on the name
+topics is a list of strings
+"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    uses update_one to update the parameter
+    "dict parameter"
+    """
+    mongo_collection.update_one({ "name": name }, { "$set": { "topics": topics } })
