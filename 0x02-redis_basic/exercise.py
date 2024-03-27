ab3868380a5f9856f73015df48b27731933d5dd7
71Arid <no232el@gmail.com>
2024-03-27 22:47:30 +0300

    type conversion

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ client as a private variable named _redis (using redis.Redis())
 and flushthe instance using flushdb.
 """
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 import uuid
 
 
@@ -32,3 +32,44 @@ class Cache:
         rnd_key = uuid.uuid4()
         self._redis.set(str(rnd_key), data)
         return str(rnd_key)
+
+    def get(self, key: str, fn: Optional[Callable]) -> Union[str, bytes, int, None]:
+        """
+        This callable will be used to convert the data
+        back to the desired format.
+        Remember to conserve the original Redis.get behavior
+        if the key does not exist.
+        """
+        v = self._redis.get(key)
+        if v is None:
+            return None
+        if fn is not None:
+            return fn(v)
+        return v
+
+    def get_str(self, key: str) -> Union[str, bytes, None]:
+        """
+        auto parametize cache.get with the correct
+        coversion function for str type
+        """
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: int) -> Union[int, bytes, None]:
+        """
+        auto parametize cache.get with the correct
+        coversion function for int type
+        """
+        return self.get(key, fn=int)
+
+
+if __name__ == "__main__":
+    cache = Cache()
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
+
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
