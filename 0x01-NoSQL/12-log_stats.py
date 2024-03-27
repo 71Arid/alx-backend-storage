eb3bcc78d76cabf17ad662bab0b63854c0b7e2d7
71Arid <no232el@gmail.com>
2024-03-27 11:19:08 +0300

    add function

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,21 +6,34 @@ NGINX logs stored in mongo db
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-logs_collection = client.logs.nginx
-count = logs_collection.count_documents({})
-print("{} logs".format(count))
-print("Methods:")
-count_get = logs_collection.count_documents({"method": "GET"})
-count_post = logs_collection.count_documents({"method": "POST"})
-count_put = logs_collection.count_documents({"method": "PUT"})
-count_patch = logs_collection.count_documents({"method": "PATCH"})
-count_delete = logs_collection.count_documents({"method": "DELETE"})
-print("\tmethod GET: {}".format(count_get))
-print("\tmethod POST: {}".format(count_post))
-print("\tmethod PUT: {}".format(count_put))
-print("\tmethod PATCH: {}".format(count_patch))
-print("\tmethod DELETE: {}".format(count_delete))
-count_status = logs_collection\
+def nginxstats():
+    """
+    connects to nginx collections in logs db
+    counts the number of documents and displays them
+    counts the number of documents with reference to
+    method types and displays the number
+    displays the no of documents with method GET
+    and path /status
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+    count = logs_collection.count_documents({})
+    print("{} logs".format(count))
+    print("Methods:")
+    count_get = logs_collection.count_documents({"method": "GET"})
+    count_post = logs_collection.count_documents({"method": "POST"})
+    count_put = logs_collection.count_documents({"method": "PUT"})
+    count_patch = logs_collection.count_documents({"method": "PATCH"})
+    count_delete = logs_collection.count_documents({"method": "DELETE"})
+    print("\tmethod GET: {}".format(count_get))
+    print("\tmethod POST: {}".format(count_post))
+    print("\tmethod PUT: {}".format(count_put))
+    print("\tmethod PATCH: {}".format(count_patch))
+    print("\tmethod DELETE: {}".format(count_delete))
+    count_status = logs_collection\
         .count_documents({"method": "GET", "path": "/status"})
-print("{} status check".format(count_status))
+    print("{} status check".format(count_status))
+
+
+if __name__ == "__main__":
+    nginxstats()
