ca531a03f3919c63d7189823daae941d4369d1b3
71Arid <no232el@gmail.com>
2024-03-30 09:40:10 +0300

    reverted to old function

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,43 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
-import redis
+"""
+In this tasks, we will implement a get_page function
+(prototype: def get_page(url: str) -> str:). The core of the
+function is very simple. It uses the requests module to
+obtain the HTML content of a particular URL and returns it.
+"""
 import requests
 from functools import wraps
 from typing import Callable
+import redis
 
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
+redis_inst = redis.Redis()
+"""global instance of redis.
+"""
 
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
+def count_calls(method: Callable) -> Callable:
+    """
+    implements the count functionality recording each
+    time a specific url is called
+    """
     @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+    def wrapper(url):
+        redis_inst.incr(f"count:{url}")
+        res = redis_inst.get(f"result:{url}")
+        if res:
+            return res.decode('utf-8')
+        res = method(url)
+        redis_inst.setex(f"result:{url}", 10, res)
+        return res
+    return wrapper
 
 
-@data_cacher
+@count_calls
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
+    """
+    use requests.get to get the contents from
+    the url and then set expitation time of
+    a cache value it input in redis
+    """
     return requests.get(url).text
