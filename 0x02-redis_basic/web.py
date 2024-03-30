7cae15867c0ecb4b923dbf448d79f9c3797a4ab8
71Arid <no232el@gmail.com>
2024-03-30 10:07:51 +0300

    reverted to oldd code

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,30 +1,43 @@
 #!/usr/bin/env python3
-"""Implement an expiring web cache and tracker"""
-from datetime import timedelta
-from typing import Callable
+"""
+In this tasks, we will implement a get_page function
+(prototype: def get_page(url: str) -> str:). The core of the
+function is very simple. It uses the requests module to
+obtain the HTML content of a particular URL and returns it.
+"""
 import requests
 from functools import wraps
+from typing import Callable
 import redis
 
 
-def cache_track_response(method: Callable):
-    """Cache and track function response"""
+redis_inst = redis.Redis()
+"""global instance of redis.
+"""
 
+
+def count_calls(method: Callable) -> Callable:
+    """
+    implements the count functionality recording each
+    time a specific url is called
+    """
     @wraps(method)
-    def wrapper(url: str, *args, **kwargs):
-        """Implement the caching and tracking"""
-        count_key = "count:{}".format(url)
-        _redis = redis.Redis()
-        _redis.incr(count_key)
-        result = method(url, *args, **kwargs)
-        _redis.setex(url, timedelta(seconds=10), str(result))
-        return result
-
+    def wrapper(url):
+        redis_inst.incr(f"count:{url}")
+        res = redis_inst.get(f"result:{url}")
+        if res:
+            return res.decode('utf-8')
+        res = method(url)
+        redis_inst.setex(f"result:{url}", 10, res)
+        return res
     return wrapper
 
 
-@cache_track_response
+@count_calls
 def get_page(url: str) -> str:
-    """Return HTML content of the `url` specified"""
-    response = requests.get(url)
-    return response.text
+    """
+    use requests.get to get the contents from
+    the url and then set expitation time of
+    a cache value it input in redis
+    """
+    return requests.get(url).text
